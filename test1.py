366c3c894add502ed91da70c14bfee231e7aaa51
Park-Sangcheon <sangcheon@ajou.ac.kr>
2024-05-19 17:19:30 +0900

    test1
    venv, test1,2

---
+++ test1.py
@@ -0,0 +1,25 @@
+import os
+import pybullet as p
+import time
+import pybullet_data
+
+
+# p.connect(p.GUI)
+# pandaUid = p.loadURDF(os.path.join(pybullet_data.getDataPath(),"franka_panda/panda.urdf"),useFixedBase=True)
+#
+# while True:
+#     p.stepSimulation()
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+startPos = [0,0,1]
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
+#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+for i in range (10000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos,cubeOrn)
+p.disconnect()
