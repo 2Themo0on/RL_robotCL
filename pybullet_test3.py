80fa01449ac5af9a20dfb1fbb7dba9c2ba3354ed
Park-Sangcheon <sangcheon@ajou.ac.kr>
2024-05-19 18:52:35 +0900

    URDF 폴더 생성, robot2 파일 추가

--- pybullet_test3.py
+++ pybullet_test3.py
@@ -6,10 +6,10 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.8)
 p.setTimeStep(1/200)
-planeID = p.loadURDF("samurai.urdf")
+planeID = p.loadURDF("plane.urdf")
 cubeStartPos = [2, -1, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 3.14])
-robotID = p.loadURDF("robot2.urdf", cubeStartPos, cubeStartOrientation)
+robotID = p.loadURDF("urdf/robot2.urdf", cubeStartPos, cubeStartOrientation)
 #robot 청소기 urdf file path 내에 있어야 load 가능
 
 
